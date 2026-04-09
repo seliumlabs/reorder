3315601a0f7bb38f817918be12984b24e3468265
Pete Hayes <pete@hayes.id.au>
2026-04-09 17:58:39 +1000

    Remove old regression tests

--- tests/inputs/last_item.rs
+++
@@ -1,7 +0,0 @@
-use uuid::Uuid;
-
-pub type RunId = Uuid;
-
-pub struct Foo {
-    bar: i32,
-}
--- tests/inputs/types.rs
+++
@@ -1,8 +0,0 @@
-use uuid::Uuid;
-
-pub type RunId = Uuid;
-pub type ArtifactId = Uuid;
-pub type TransitionId = &'static str;
-pub type ValidatorId = &'static str;
-pub type ExecutorId = &'static str;
-pub type FindingId = Uuid;
